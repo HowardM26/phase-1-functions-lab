4ed8fbc1942540180671f5671391b3159a260b1a
+ <+>
2023-04-10 21:55:25 -0400

    phase-1 functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    const hqLocation = 42 
+    return Math.abs(someValue - hqLocation)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(start - destination);
+    return distanceInBlocks * 264;
+}
+
+function calculatesFarePrice (start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    }
+    else if (distance>=400 && distance<=2000){
+        return Math.abs(distance-400)*0.02;
+    }
+    else if(distance>=2000 && distance<=2500){
+        return 25;
+    }
+    else if (distance > 2500){
+        return 'cannot travel that far';
+    }
+}
